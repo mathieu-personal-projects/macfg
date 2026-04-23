df71977f40f75eb47273157f6a73bf048e935f16
MathieuAudibert <mathieu.audibert27@gmail.com>
2026-04-23 21:40:16 +0200

    feat: logs banner resources

--- config/src/main/kotlin/mpp/macfg/config/ConfigApplication.kt
+++ config/src/main/kotlin/mpp/macfg/config/ConfigApplication.kt
@@ -2,10 +2,17 @@ package mpp.macfg.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.github.oshai.kotlinlogging.KotlinLogging
 
 @SpringBootApplication
 class ConfigApplication
 
 fun main(args: Array<String>) {
+	val logger = KotlinLogging.logger {}
+
+	logger.trace { "Starting Kotlin Springboot application" }
 	runApplication<ConfigApplication>(*args)
+	
+	val banner = ConfigApplication::class.java.getResource("/static/banner.txt")?.readText() ?: ""
+	logger.info { "\n$banner\nMaCfg • V0.1.0 • Configuration setupper" }
 }
